9bfba0e0628f30b775978376a716160552bb8ae6
Dhiraj <dhiraj.jamia@gmail.com>
2018-12-09 00:38:19 +0530

    Update Demo_Scrapy.R

--- Demo_Scrapy.R
+++ Demo_Scrapy.R
@@ -10,3 +10,8 @@ test_html
 selector<-".blurb"
 text<-test_html %>% html_node(css=selector) %>% html_text()
 text
+#Scraping table from web
+data_table<-read_html("http://www.michaeljgrogan.com/rvest-web-scraping-using-r/")
+selector<-".column-4 , .column-3 , .column-2 , .column-1"
+my_data<-data_table %>% html_nodes(css=selector) %>% html_text()
+my_data
